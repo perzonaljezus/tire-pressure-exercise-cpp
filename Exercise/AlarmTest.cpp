0ea8ceb0d1e128ce5f53f5f136abf04decb1f63e
Ben Wu <wubin28@gmail.com>
2014-07-11 21:54:27 +0800

    Wrote the intent code for the Act and Arrange parts of test a_normal_pressure_value_should_not_raise_the_alarm.

--- Exercise/AlarmTest.cpp
+++ Exercise/AlarmTest.cpp
@@ -3,6 +3,14 @@
  
 // TODO-intent-working-on: a normal pressure value should not raise the alarm
 TEST(AlarmTest, a_normal_pressure_value_should_not_raise_the_alarm) {
+    // Arrange
+    StubSensor *stubSensor = new StubSensor;
+    stubSensor->arrangeReturnPressureValue(Alarm::HIGH_PRESSURE_THRESHOLD);
+    Alarm *alarm = new Alarm(stubSensor);
+    
+    // Act
+    alarm->check();
+    
     // Assert
     EXPECT_FALSE(alarm->isAlarmOn());
 }
