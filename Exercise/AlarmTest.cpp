6e4994742baa94b5636a3a704b23cf26db2b77b7
Ben Wu <wubin28@gmail.com>
2014-07-11 19:32:55 +0800

    Added 2 intent TODOs.

--- Exercise/AlarmTest.cpp
+++ Exercise/AlarmTest.cpp
@@ -5,4 +5,6 @@ TEST(AlarmTest, a_trivial_test) {
     EXPECT_EQ(1, 3-2);
 }
 
+// TODO-intent: a normal pressure value should not raise the alarm
+// TODO-intent: a pressure value outside the range should raise the alarm
 // TODO-new-intent: a normal pressure value after a value outside the range should stop the alarm
