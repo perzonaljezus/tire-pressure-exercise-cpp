18264962d560112009bdc51fdd80bef95b339b73
Ben Wu <wubin28@gmail.com>
2014-07-12 08:28:18 +0800

    Wrote the intent code of the Act and Arrange parts of the test a_normal_pressure_value_after_a_value_outside_the_range_should_stop_the_alarm.

--- Exercise/AlarmTest.cpp
+++ Exercise/AlarmTest.cpp
@@ -30,6 +30,15 @@ TEST(AlarmTest, a_pressure_value_outside_the_range_should_raise_the_alarm) {
   
 // TODO-new-intent-working-on: a normal pressure value after a value outside the range should stop the alarm
 TEST(AlarmTest, a_normal_pressure_value_after_a_value_outside_the_range_should_stop_the_alarm) {
+  // Arrange
+  StubSensor *stubSensor = new StubSensor;
+  stubSensor->arrangeReturnPressureValues(Alarm::LOW_PRESSURE_THRESHOLD
+    , Alarm::LOW_PRESSURE_THRESHOLD - 1, Alarm::LOW_PRESSURE_THRESHOLD);
+  Alarm *alarm = new Alarm(stubSensor);
+  
+  // Act
+  alarm->check();
+  
   // Assert
   EXPECT_FALSE(alarm->isAlarmOn());
 }
