d691a32c1f62e4563dfa68c38e0670d7de4d5bbd
Ben Wu <wubin28@gmail.com>
2014-07-12 08:00:03 +0800

    Working on TODO: a pressure value outside the range should raise the alarm.

--- Exercise/AlarmTest.cpp
+++ Exercise/AlarmTest.cpp
@@ -2,7 +2,6 @@
 #include "Alarm.h"
 #include "StubSensor.h"
  
-// TODO-intent-working-on: a normal pressure value should not raise the alarm
 TEST(AlarmTest, a_normal_pressure_value_should_not_raise_the_alarm) {
     // Arrange
     StubSensor *stubSensor = new StubSensor;
@@ -16,5 +15,5 @@ TEST(AlarmTest, a_normal_pressure_value_should_not_raise_the_alarm) {
     EXPECT_FALSE(alarm->isAlarmOn());
 }
 
-// TODO-intent: a pressure value outside the range should raise the alarm
+// TODO-intent-working-on: a pressure value outside the range should raise the alarm
 // TODO-new-intent: a normal pressure value after a value outside the range should stop the alarm
